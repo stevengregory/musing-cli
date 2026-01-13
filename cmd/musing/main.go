42a2943905cab3c99c04ab5934caa3e9c72b5fef
Steven Gregory <sgregory@gmail.com>
2026-01-13 12:28:07 -0600

    fix: inject ldflags directly into cmd.Version instead of main.Version

--- cmd/musing/main.go
+++ cmd/musing/main.go
@@ -4,18 +4,6 @@ import (
 	"github.com/stevengregory/musing-cli/cmd"
 )
 
-var (
-	version = "0.1.0" // Updated by ldflags during release
-	commit  = "none"
-	date    = "unknown"
-)
-
 func main() {
-	// Set version information
-	cmd.Version = version
-	cmd.Commit = commit
-	cmd.Date = date
-
-	// Execute root command
 	cmd.Execute()
 }
